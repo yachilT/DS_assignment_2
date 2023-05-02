66265aba7ba8fe6d4a19d3ebb4d4ae84cdd821c7
Roi Ohayon <roioha@bgu.post.ac.il>
2023-05-02 08:50:37 +0300

    Almost Done

--- src/Container.java
+++ src/Container.java
@@ -4,12 +4,20 @@ import java.util.Comparator;
 public class Container{
 	private Point data;//Don't delete or change this field;
 	private Comparator<Point> cmpr;
+	private Container prev;
+	private Container next;
 
-	public Container(Point data, Comparator<Point> cmpr) {
+	private Container otherAxis;
+
+	public Container(Point data, Comparator<Point> cmpr, Container prev, Container next, Container otherAxis) {
 		this.data = data;
 		this.cmpr = cmpr;
+		this.next = next;
+		this.prev = prev;
+		this.otherAxis = otherAxis;
 	}
-	
+
+
 	//Don't delete or change this function
 	public Point getData()
 	{
@@ -22,5 +30,50 @@ public class Container{
 
 	public Comparator<Point> getCmpr() {
 		return cmpr;
+	}
+
+	public Container getPrev() {
+		return prev;
+	}
+
+	public void setPrev(Container prev) {
+		this.prev = prev;
+	}
+
+	public Container getNext() {
+		return next;
+	}
+
+	public void setNext(Container next) {
+		this.next = next;
+	}
+
+	public Container getOtherAxis() {
+		return otherAxis;
+	}
+
+	public void setOtherAxis(Container otherAxis) {
+		this.otherAxis = otherAxis;
+	}
+
+
+	public boolean hasNext() {
+		return this.next != null;
+	}
+
+	public boolean hasPrev() {
+		return this.prev != null;
+	}
+
+	public void delete() {
+		if (this.hasNext())
+			this.next.prev = this.prev;
+
+		if (this.hasPrev())
+			this.prev.next = this.next;
+
+		this.next = null;
+		this.prev = null;
+		this.otherAxis = null;
 	}
 }
--- src/DataStructure.java
+++ src/DataStructure.java
@@ -1,13 +1,16 @@
 import javax.crypto.Cipher;
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class DataStructure implements DT {
 
-	private Link<Container> xFirst;
-	private Link<Container> xLast;
-	private Link<Container> yFirst;
-	private Link<Container> yLast;
+	private Container xFirst;
+	private Container xLast;
+	private Container yFirst;
+	private Container yLast;
 	private int size;
+
+
 
 	//////////////// DON'T DELETE THIS CONSTRUCTOR ////////////////
 	public DataStructure() {
@@ -18,19 +21,26 @@ public class DataStructure implements DT {
 		this.size = 0;
 	}
 
+	public DataStructure(Container xFirst, Container xLast, Container yFirst, Container yLast, int size) {
+		this.xFirst = xFirst;
+		this.xLast = xLast;
+		this.yFirst = yFirst;
+		this.yLast = yLast;
+		this.size = size;
+	}
+
 	@Override
 	public void addPoint(Point point) {
 		if (size == 0) {
-			xFirst = new Link<>(new Container(point, new XComparator()), null, null, null);
+			xFirst = new Container(point, new XComparator(), null, null, null);
 			xLast = xFirst;
-			yFirst = new Link<>(new Container(point, new YComparator()), null, null, xFirst);
+			yFirst = new Container(point, new YComparator(), null, null, xFirst);
 			yLast = yFirst;
-
 			xFirst.setOtherAxis(yFirst);
 		} else {
 
-			Link<Container> toAddX = new Link<>(new Container(point, new XComparator()), null, null, null);
-			Link<Container> toAddY = new Link<>(new Container(point, new YComparator()), null, null, toAddX);
+			Container toAddX = new Container(point, new XComparator(), null, null, null);
+			Container toAddY = new Container(point, new YComparator(), null, null, toAddX);
 			toAddX.setOtherAxis(toAddY);
 
 			add(toAddX, xFirst);
@@ -51,13 +61,13 @@ public class DataStructure implements DT {
 		size++;
 	}
 
-	private void add(Link<Container> toAdd, Link<Container> curr) {
-		Point point = toAdd.getData().getData();
-		while (curr.hasNext() && curr.getData().compareTo(point) < 0) {
+	private void add(Container toAdd, Container curr) {
+		Point point = toAdd.getData();
+		while (curr.hasNext() && curr.compareTo(point) < 0) {
 			curr = curr.getNext();
 		}
 
-		if (curr.getData().compareTo(point) > 0) {
+		if (curr.compareTo(point) > 0) {
 			toAdd.setPrev(curr.getPrev());
 			toAdd.setNext(curr);
 		} else {
@@ -78,14 +88,14 @@ public class DataStructure implements DT {
 		Point[] points = new Point[size];
 		int index = 0;
 
-		Link<Container> curr = axis ? xFirst : yFirst;
+		Container curr = axis ? xFirst : yFirst;
 		Point minPoint = new Point(min,min);
 		Point maxPoint = new Point(max,max);
 
 
-		while (curr.hasNext() & curr.getData().compareTo(maxPoint) < 0) {
-			if (curr.getData().compareTo(minPoint) > 0) {
-				points[index] = curr.getData().getData();
+		while (curr.hasNext() & curr.compareTo(maxPoint) < 0) {
+			if (curr.compareTo(minPoint) > 0) {
+				points[index] = curr.getData();
 				index++;
 			}
 		}
@@ -103,22 +113,22 @@ public class DataStructure implements DT {
 
 	@Override
 	public double getDensity() {
-		return (double)(size) / ((xLast.getData().getData().getX() - xFirst.getData().getData().getX()) *
-				(yLast.getData().getData().getY() - yFirst.getData().getData().getY()));
+		return (double)(size) / ((xLast.getData().getX() - xFirst.getData().getX()) *
+				(yLast.getData().getY() - yFirst.getData().getY()));
 	}
 
 	@Override
 	public void narrowRange(int min, int max, Boolean axis) {
-		Link<Container> first = axis ? xFirst : yFirst;
-		Link<Container> last = axis ? xLast : yLast;
+		Container first = axis ? xFirst : yFirst;
+		Container last = axis ? xLast : yLast;
 
 		Point minPoint = new Point(min,min);
 		Point maxPoint = new Point(max,max);
 
 		while((first.hasNext() & last.hasPrev())
-				&& first.getData().compareTo(minPoint) < 0 & last.getData().compareTo(maxPoint) > 0){
-			Link<Container> toRemoveFirst = first;
-			Link<Container> toRemoveLast = last;
+				&& first.compareTo(minPoint) < 0 & last.compareTo(maxPoint) > 0){
+			Container toRemoveFirst = first;
+			Container toRemoveLast = last;
 
 			first = first.getNext();
 			last = last.getPrev();
@@ -127,14 +137,14 @@ public class DataStructure implements DT {
 			delete(toRemoveLast);
 		}
 
-		while (first.hasNext() && first.getData().compareTo(minPoint) < 0) {
-			Link<Container> toRemoveFirst = first;
+		while (first.hasNext() && first.compareTo(minPoint) < 0) {
+			Container toRemoveFirst = first;
 			first = first.getNext();
 			delete(toRemoveFirst);
 		}
 
-		while (last.hasPrev() && last.getData().compareTo(maxPoint) > 0) {
-			Link<Container> toRemoveLast = last;
+		while (last.hasPrev() && last.compareTo(maxPoint) > 0) {
+			Container toRemoveLast = last;
 			last = last.getPrev();
 			delete(toRemoveLast);
 		}
@@ -143,33 +153,124 @@ public class DataStructure implements DT {
 
 	@Override
 	public Boolean getLargestAxis() {
-		return xLast.getData().getData().getX() - xFirst.getData().getData().getX() >
-				yLast.getData().getData().getY() - yFirst.getData().getData().getY();
+		return xLast.getData().getX() - xFirst.getData().getX() >
+				yLast.getData().getY() - yFirst.getData().getY();
 	}
 
 	@Override
 	public Container getMedian(Boolean axis) {
-		Link<Container> curr = axis ? xFirst : yFirst;
+		Container curr = axis ? xFirst : yFirst;
 		int index = 0;
 		while (index < this.size){
 			curr = curr.getNext();
 		}
-		return curr.getData();
+		return curr;
 	}
 
 	@Override
 	public Point[] nearestPairInStrip(Container container, double width,
 									  Boolean axis) {
-		// TODO Auto-generated method stub
-		return null;
+
+		Point[] inStrip = getPointsInStrip(container,width,axis);
+		Arrays.sort(inStrip,axis ? new YComparator() : new XComparator());
+
+		Point[] nearestPoints = new Point[2];
+		double minDist = Double.POSITIVE_INFINITY;
+		for (int i = 0; i < inStrip.length; i++) {
+			for (int j = i + 1; j < i + 8 & j < inStrip.length; j++) {
+				double currDist = dist(inStrip[i], inStrip[j]);
+				if (currDist < minDist){
+					nearestPoints[0] = inStrip[i];
+					nearestPoints[1] = inStrip[j];
+					minDist = currDist;
+				}
+
+			}
+		}
+		return nearestPoints;
+	}
+
+	private Point[] getPointsInStrip(Container container, double width, boolean axis) {
+		int maxVal = (int)Math.floor(container.getData().getX() + width / 2 + 1);
+		Point upperBound = new Point(maxVal, maxVal);
+		int minVal = (int)Math.ceil(container.getData().getX() - width / 2 - 1);
+		Point lowerBound = new Point(minVal, minVal);
+
+		Point[] arr = new Point[size];
+		if ((axis & xLast.compareTo(upperBound) <= 0 & xFirst.compareTo(lowerBound) >= 0) |
+				!axis & yLast.compareTo(upperBound) <= 0 & yFirst.compareTo(lowerBound) >= 0) {
+			Container curr = axis ? yFirst: xFirst;
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = curr.getData();
+				curr = curr.getNext();
+			}
+			return  arr;
+		}
+
+
+		Container prev = container.getPrev();
+		Container next = container.getNext();
+
+
+		arr[0] = container.getData();
+		int index = 1;
+
+		while (prev.compareTo(lowerBound) >= 0 & next.compareTo(upperBound) <= 0) {
+			arr[index] = prev.getData();
+			arr[index + 1] = next.getData();
+			index += 2;
+		}
+
+		while (prev.compareTo(lowerBound) >= 0) {
+			arr[index] = prev.getData();
+			index++;
+		}
+
+		while (next.compareTo(upperBound) <= 0) {
+			arr[index] = next.getData();
+			index++;
+		}
+		Point[] inStrip = new Point[index];
+
+		for (int i = 0; i < inStrip.length; i++) {
+			inStrip[i] = arr[i];
+		}
+
+		return inStrip;
+	}
+
+	public double dist(Point p1,Point p2){
+		return Math.sqrt(Math.pow(p1.getX() - p2.getX(),2) + Math.pow(p1.getY() - p2.getY(),2));
 	}
 
 	@Override
 	public Point[] nearestPair() {
-		// TODO Auto-generated method stub
-		return null;
+		if(size <= 2)
+			return size < 2 ? new Point[2] : new Point[]{xFirst.getData(),xLast.getData()};
+
+
 	}
-	private void delete(Link<Container> toDelete){
+
+	public double nearestPair(Point[] arr,int left,int right, Point[] nearest){
+		Point[] pair = new Point[2];
+		if(right + 1 - left <= 3) {
+			double minDistance = Double.POSITIVE_INFINITY;
+			for (int i = 0; i < arr.length; i++)
+				for (int j = i + 1; j < arr.length; j++)
+					if(dist(arr[i],arr[j]) < minDistance) {
+						pair[0] = arr[i];
+						pair[1] = arr[j];
+						minDistance = dist(arr[i],arr[j]);
+			}
+		}
+		else{
+
+		}
+
+
+	}
+
+	private void delete(Container toDelete){
 		toDelete.getOtherAxis().delete();
 		toDelete.delete();
 		this.size--;
